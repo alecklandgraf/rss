bd0b750f4dee7b5003e6ed3fb5c5f197ab2dca8d
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2020-01-19 15:16:06 -0800

    Working display

--- index.mjs
+++ index.mjs
@@ -1,21 +1,50 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
-const FEED = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCLsiaNUb42gRAP7ewbJ0ecQ'
+const FEED =
+  "https://www.youtube.com/feeds/videos.xml?channel_id=UCLsiaNUb42gRAP7ewbJ0ecQ";
 
-async function parseFeed(feed) {
-  if (typeof feed !== 'string' || feed === '') {
-    throw new Error('Feed must be a valid url (string)')
-  }
-
-  const data = await fetch(feed);
-  const text = await data.text();
-  console.log(text.split('\n').slice(0,5))
-
-  // extract title via regex
-  // const rxTitle = /<title>(.*)<\/title>/g;
-  // const matches = [...text.matchAll(rxTitle)]
-  // matches.forEach(match => console.log(match[1]))
-  // console.log(matches.length)
+function pad(str, indentation) {
+  return str.padStart(str.length + indentation);
 }
 
-parseFeed(FEED).then()
+async function parseFeed(feedUrl) {
+  if (typeof feedUrl !== "string" || feedUrl === "") {
+    throw new Error("Feed must be a valid url (string)");
+  }
+  console.log(`Fetching feed for url: ${feedUrl}\n\n`);
+
+  const data = await fetch(feedUrl);
+  // future optimization to use the body ReadableStream and use less mem
+  const text = await data.text();
+  const lines = text.split("\n");
+
+  const feed = {};
+
+  let parent = feed;
+  let lastToken = [];
+
+  let indendation = 0;
+  const DEFAULT_SPACES = 2;
+
+  lines.splice(0, 150).forEach(line => {
+    const [, token] = line.match(/<([\w:]+)/) || [];
+    if (token) {
+      if (token === "title") {
+        console.log(pad(`${token}: ${line}`, indendation));
+      } else {
+        console.log(pad(token, indendation));
+      }
+      if (line.endsWith(`/${token}>`) || line.endsWith("/>")) {
+        // add context
+      } else {
+        lastToken.unshift(token);
+        indendation += DEFAULT_SPACES;
+      }
+    } else if (line.match(`</${lastToken[0]}>`)) {
+      indendation -= DEFAULT_SPACES;
+      lastToken.shift();
+    }
+  });
+}
+
+parseFeed(FEED).then();
